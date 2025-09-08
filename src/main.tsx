75ab379c66e78bdfcbb53e3acb4732858bc4a30e
Chinoros <chinoros2815@gmail.com>
2025-09-08 11:56:09 +0700

    push

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import { MantineProvider, createTheme, virtualColor } from "@mantine/core";
 const theme = createTheme({
   fontFamily: "Kanit,sans-serif",
   colors: {
-    chanadda: virtualColor({
-      name: "chanadda",
+    chinoros: virtualColor({
+      name: "chinoros",
       dark: "yellow",
       light: "violet",
     }),
